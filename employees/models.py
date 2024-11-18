f252d2971610ba527c9579cd5642cef07598aeb7
puraze <Alexandremiqadze@gmail.com>
2024-11-18 19:26:08 +0400

    Add files via upload

---
+++ employees/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
+    salary = models.IntegerField()
